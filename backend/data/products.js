bcc57fa06156dee519155b90665a2ca58607d78f
Sharma Ketan <sharmaketann@gmail.com>
2022-01-13 05:30:23 +0530

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    name: 'Onions',
+    image: '/images/onion.png',
+    description:
+      'Onions are an essential part of any kitchen, and the Overseas Trade House has the best in bulk – get yours today!',
+    category: 'Vegetable',
+    price: 20,
+    countInStock: 30,
+    numReviews: 4,
+    rating: 4.0,
+  },
+  {
+    name: 'Eggplant',
+    image: '/images/eggplant.png',
+    description:
+      'Onions are an essential part of any kitchen, and the Overseas Trade House has the best in bulk – get yours today!',
+    category: 'Vegetable',
+    price: 40,
+    countInStock: 30,
+    numReviews: 4,
+    rating: 4.5,
+  },
+  {
+    name: 'Potato',
+    image: '/images/potato1.png',
+    description:
+      'Onions are an essential part of any kitchen, and the Overseas Trade House has the best in bulk – get yours today!',
+    category: 'Vegetable',
+    price: 20,
+    countInStock: 30,
+    numReviews: 4,
+    rating: 4.0,
+  },
+  {
+    name: 'Coriander',
+    image: '/images/coriander.png',
+    description:
+      'Onions are an essential part of any kitchen, and the Overseas Trade House has the best in bulk – get yours today!',
+    category: 'Vegetable',
+    price: 10,
+    countInStock: 30,
+    numReviews: 4,
+    rating: 3.0,
+  },
+  {
+    name: 'Coriander',
+    image: '/images/coriander.png',
+    description:
+      'Onions are an essential part of any kitchen, and the Overseas Trade House has the best in bulk – get yours today!',
+    category: 'Vegetable',
+    price: 10,
+    countInStock: 0,
+    numReviews: 4,
+    rating: 3.5,
+  },
+  {
+    name: 'Coriander',
+    image: '/images/coriander.png',
+    description:
+      'Onions are an essential part of any kitchen, and the Overseas Trade House has the best in bulk – get yours today!',
+    category: 'Vegetable',
+    price: 10,
+    countInStock: 30,
+    numReviews: 2,
+    rating: 2.5,
+  },
+]
+
+export default products
